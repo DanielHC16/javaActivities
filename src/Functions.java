715b385b96850b4bbe11db4222e01978431fcc14
Daniel Camacho <danielcamacho0416@gmail.com>
2024-09-08 09:11:11 +0800

    Added Iterative and Recursive Functions

--- src/Functions.java
+++ src/Functions.java
@@ -23,8 +23,107 @@ public class Functions{
             return reccirclearea(dblRadius - 1) + Math.PI;
     }
 
+    // 2. Display the sum of the below series given n as the number of terms:
+    // 2 + 4 + 6 + 8 + 10 + ...
+
+    // 2A. Iterative Function
+    public int itesumseries(int intNum1){
+        int intSum = 0;
+        for(int intCounter = 1; intCounter <= intNum1; intCounter++){
+            intSum += intCounter *2;
+        }
+        return intSum;
+    }
+
+    // 2B. Recursive Function
+    public int recsumseries(int intNum1){
+        if (intNum1 == 0){
+        return 0;
+       }
+        int termVal = intNum1 * 2;
+    return termVal + recsumseries(intNum1 - 1);
+    }
     
+    // 3. Given N as an input, display the number of digits it has.
+
+    // 3A. Iterative Function
+    public int itecondigit(int intNum1){
+        if(intNum1 == 0)
+            return 1;
+        
+        int intCounter = 0;
+
+        while (intNum1 > 0){
+            intNum1 /= 10;
+            intCounter++;
+        }
+        return intCounter;
+    } 
+
+    // 3B. Recursive Function
+    public int reccountdigit(int intNum1){
+        if (intNum1 == 0 || intNum1 < 10){
+            return 1;
+        }
+        return 1 + reccountdigit(intNum1 / 10);
+    }
+
+    // 4. Given N as an input, display the reversed digits of N.
+
+    // 4A. Iterative Function
+        
+
+    public int itereverseddigits(int intNum){
+        int intReverse = 0;
+
+        while (intNum != 0){
+            intReverse = (intReverse *10) + intNum%10;
+            intNum = intNum/10;
+        }
+
+        return intReverse;
+    }
+
+    // 4B. Recursive Function
+    
+    public int recreverseddigits(int intNum, int intReverse){
+        if (intNum == 0)
+            return intReverse;
+        
+        intReverse = (intReverse * 10) + intNum%10;
+
+        return recreverseddigits(intNum/10, intReverse);
+    }
+
+    // 5. Given an array of characters, tell whether a search key is found on the array.
+    // Ex. Array = "hello"
+    // search key = "e'
+    /// e is found in the array
+
+    // 5A. Iterative Function
+    public boolean itesearchkey(char keyToSearch, char[] arrayOfChar){
+        for(char charElement : arrayOfChar){
+            if(keyToSearch == charElement)
+                return true;
+        }
+        return false;
+    }
+
+    // 5B. Recursive Function
+    public boolean recsearchkey(char keyToSearch, char[] arrayOfChar, int intCounter){
+        if(intCounter == arrayOfChar.length)
+            return false;
+        else if(arrayOfChar[intCounter] == keyToSearch)
+            return true;
+        else 
+            return recsearchkey(keyToSearch, arrayOfChar, intCounter + 1);
+    }
+
 
 
 
 }
+    
+
+
+
